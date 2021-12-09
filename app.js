64f73d455b707c4736f9e0cd27822ca373743d96
Ethaniel Botello <Ebotel200@west-mec.local>
2021-12-09 15:26:14 -0700

    controllers and middleware setup

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const fileUpload = require("express-fileupload")
 
 
 const connectDB = require("./db/connect");
+
 const productsRouter = require('./routes/productsRouter');
+const cartRouter = require('./routes/cartRouter')
 
 const notFoundError = require("./middleware/notFound");
 const errorHandlerMiddleware = require("./middleware/errorHandler");
@@ -19,10 +21,11 @@ app.use(fileUpload({useTempFiles: true}))
 
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.send(`<h1>fsdfasd</h1>`)
+app.get('/*', (req, res) => {
+    res.send(`<h1>OOPSIE WOOPSIE!! uwU We made a wittle fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!</h1>`)
 })
 app.use("/api/v1/products", productsRouter)
+// app.use("/api/v1/cart", cartRouter)
 
 app.use(notFoundError)
 app.use(errorHandlerMiddleware)
--- controllers/productController.js
+++ controllers/productController.js
@@ -2,9 +2,7 @@ const Product = require("../Models/Product");
 const { StatusCodes } = require("http-status-codes");
 
 const createProduct = async (req, res) => {
-  const {
-    params: { name: productName },
-  } = req;
+  const productName = req.body.name;
 
   const product = await Product.findOne({
     name: productName,
@@ -12,14 +10,17 @@ const createProduct = async (req, res) => {
 
   if (!product) {
     const createdProduct = await Product.create(req.body);
+    // console.log(`productName: ${productName}, product: ${product.name}`);
     res.status(200).json({ createdProduct });
+  } else {
+    throw new Error(`Product with name ${product.name} already exists`);
   }
 };
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find({});
-    res.status(200).json({ products })
-}
+  const products = await Product.find({});
+  res.status(200).json({ products });
+};
 
 const DeleteProduct = async (req, res) => {
   const {
@@ -35,7 +36,7 @@ const DeleteProduct = async (req, res) => {
   }
 
   res.status(StatusCodes.OK).json({ product });
-}
+};
 
 const getProduct = async (req, res) => {
   const { id: productID } = req.params;
@@ -48,7 +49,7 @@ const getProduct = async (req, res) => {
     throw new NotFoundError(`no product with the id of ${productID}`);
   }
 
-  res.status(StatusCodes.OK).json({ product });};
+  res.status(StatusCodes.OK).json({ product });
+};
 
-
-module.exports = { createProduct, getAllProducts, getProduct, DeleteProduct }
+module.exports = { createProduct, getAllProducts, getProduct, DeleteProduct };
